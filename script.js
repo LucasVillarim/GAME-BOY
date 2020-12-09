06627cf007c20f9e6633e20070b47b274ab944dc
LucasVillarim <villarim94@gmail.com>
2020-12-09 02:58:14 -0300

    audio outputs in progress

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function powerOn() {
   screenElement.classList.toggle("screen-on")
   // alert("ok");
 }
+
+
+let outputsRow = document.getElementsByClassName("vertical-outputs");
+for (i = 0; i <= outputsRow.length; i++) {
+  for (j = 0; j <= 3; j++) {
+    let output = document.createElement("span");
+    output.setAttribute("class", "output");
+    outputsRow[i].appendChild(output);
+  }
+}
+
+// let outputsRowHorizontal = document.getElementsByClassName("horizontal-outputs");
+// for (i = 0; i <= outputsRow.length; i++) {
+//   for (j = 0; j <= 3; j++) {
+//     let output = document.createElement("span");
+//     output.setAttribute("class", "output");
+//     outputsRow[i].appendChild(output);
+//   }
+// }
